3f36745192cefa0b9a6e01186a63ee594cf33ef3
majidsh97 <sharghi.majid@gmail.com>
2024-06-10 19:48:27 +0200

    0.0.3

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ source myenv/bin/activate
 #vscode env
 #https://techinscribed.com/python-virtual-environment-in-vscode/
 #https://stackoverflow.com/questions/58119823/jupyter-notebooks-in-visual-studio-code-does-not-use-the-active-virtual-environm
+#python  -m ipykernel install --user --name=myenv
 pip install transformers
 pip install peft
 pip install bitsandbytes
