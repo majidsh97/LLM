45b233ee9ec8e0288a08d0913bc9a21a507bf526
majidsh97 <sharghi.majid@gmail.com>
2024-06-28 21:42:04 +0200

    .

--- utils.py
+++ utils.py
@@ -1,17 +1,17 @@
 KEYWORDS={
         #----------------------form--------------------------------------
-       'firstname':['firstname','first_name','forename','first','first-name'],
-       'lastname':['lastname','last_name','surename','last','last-name'] ,
+       'first name':['firstname','first_name','forename','first','first-name'],
+       'last name':['lastname','last_name','surename','last','last-name'] ,
        'email':['email','e-mail','username','user_name','user-name','type="email"'], 
        'password':['password'], 
        'cv':['cv','resume','curriculum','vitae'],
-       'birthdate':['birthdate','birth','dateofbirth','date-of-birth','birth-date'],
-       'phonenumber':['phonenumber','phone','telephone','tel'],
-       'housenumber':['hausnummer'],
-       'residentcountry':['resident','country'],
-       'citizencountry':['citizen','citizenship','city'],
+       'birth date':['birthdate','birth','dateofbirth','date-of-birth','birth-date'],
+       'phone number':['phonenumber','phone','telephone','tel'],
+       'house number':['hausnummer'],
+       'resident country':['resident','country'],
+       'citizen country':['citizen','citizenship','city'],
        'transcript':['transcript'],
-       'coverletter':['coverletter','cover', 'letter'], 
+       'cover letter':['coverletter','cover', 'letter'], 
        'picture':['picture'], 
        'address':['address','location'],
        'linkedin':['linkedin'],
@@ -19,36 +19,34 @@ KEYWORDS={
        'xing':['xing'],
        'twitter':['twitter'],
        'website':['website'],
-       'postalcode':['postalcode','zip','zip-code','postal'],
+       'zip':['postalcode','zip','zip-code','postal'],
        'city':['city'],
-       'housenumber':['housenumber'], 
        'salary':['salary'],
        'sex':['sex','gender'], 
-       'availablefrom':['noticeprieod'],
+       'available from':['noticeprieod'],
        #'file':['type="file"'],
        
        'captcha':['captcha'],
        'submit':['apply','type="submit"'],
        'cancel':['cancel'],
        'fake':['fake'],
-       'question':['question'],
-       'agree':['agree'],
-       'other':['other'],
-       'apply':['apply'],
-       'recommendation':['recommendation'],
-       'workherebefore':['workherebefore'],
-       'findus':['find'],
+       'custom question':['question'],
+       'privacy':['agree'],
+
+       'recommend':['recommendation'],
+       'work here before':['workherebefore'],
+       'find us':['find'],
        'dropbox':['dropbox'],
-       'googledrive':['google drive'],
-       'workhours':['workhours'],
+       'google drive':['google drive'],
+       'work hours':['workhours'],
        'workduration':[''],
        'visa':['visa'],
-       'eligible':['eligible'],
-       'germanlevel':['germanlevel'],
+       #'eligible':['eligible'],
+       'german language level':['germanlevel'],
        'company':[''],#current company
-       'relocate':[''],
-       'universityenrollment':[],#are a student?
-       'englishlevel':[],
+       'willing to relocate':[''],
+       'university enrollment':[],#are a student?
+       'english language level':[],
        #important about job
        #important about jobplace?
               
@@ -60,15 +58,14 @@ KEYWORDS={
        'cookie':['cookie'],
        'next':['apply now','next'],
        'search':[],
+        'other':['other'],
+        'newsletter':[]
+       #'apply':['apply'],
        #-------------------------------------------------------------------
-       
-       
-       
-       
+
        }
 
-
-LABEL_INDEX_TO_KEY = list(KEYWORDS.keys()) #self.personal_data.columns
+LABEL_INDEX_TO_KEY = [KEYWORDS.keys()]
 i =0
 LABEL_KEY_TO_INDEX = {}
 for key in KEYWORDS:
