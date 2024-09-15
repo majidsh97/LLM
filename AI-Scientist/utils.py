e956429a83716ec8f3c1d7fa22aafdfe7fd07c84
majidsh97 <sharghi.majid@gmail.com>
2024-09-15 11:03:17 +0200

    v1.1

--- AI-Scientist/utils.py
+++ AI-Scientist/utils.py
@@ -9,15 +9,16 @@ def extract_json_between_markers(llm_output):
         start_index += len(json_start_marker)  # Move past the marker
         end_index = llm_output.find(json_end_marker, start_index)
     else:
-        return None  # JSON markers not found
+        return "JSON markers not found"
 
     if end_index == -1:
-        return None  # End marker not found
+        return " End marker not found"
 
     # Extract the JSON string
     json_string = llm_output[start_index:end_index].strip()
     try:
         parsed_json = json.loads(json_string)
         return parsed_json
-    except json.JSONDecodeError:
-        return None 
+    except json.JSONDecodeError as e:
+        return  e.msg
+        
